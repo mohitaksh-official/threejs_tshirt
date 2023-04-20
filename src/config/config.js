dbeec2a0e52fd1fe87ac88da474fe3dac85c5ea1
Mohitaksh Srivastava <mohitaksh.official@gmail.com>
2023-04-21 00:36:09 +0530

    push

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "https://ai-image-generator-bva6.onrender.com/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
